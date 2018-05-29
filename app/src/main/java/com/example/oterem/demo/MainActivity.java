1295a51e7bfe2f2213f12dca3565190f4324d5f6
Omri Terem <oterem@cisco.com>
2018-05-29 20:39:30 +0300

    adding pop up and showcase on first use in app

--- app/src/main/java/com/example/oterem/demo/MainActivity.java
+++ app/src/main/java/com/example/oterem/demo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.oterem.demo;
 
+
 import android.content.ClipData;
+
+import android.app.Dialog;
+
 import android.content.Intent;
 
+import android.graphics.Color;
 import android.net.Uri;
 
 import android.os.Build;
@@ -11,6 +16,7 @@ import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
@@ -22,13 +28,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import android.widget.ImageView;
+
+import android.widget.Button;
+
 import android.widget.Toast;
+import com.droidbyme.dialoglib.AnimUtils;
+import com.droidbyme.dialoglib.DroidDialog;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.io.File;
 import java.io.IOException;
+
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -56,6 +73,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -70,6 +88,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        showCase("true");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.navigation, menu);
         return true;
@@ -87,6 +106,10 @@ public class MainActivity extends AppCompatActivity
             startAboutActivity();
             return true;
         }
+        if (id == R.id.action_help) {
+
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -99,15 +122,15 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-            launchCamera(item.getActionView());
+            launchCamera(null);
         } else if (id == R.id.nav_gallery) {
-            galleryBrowse(item.getActionView());
+            galleryBrowse(null);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-            Utils.tellAboutUs(this,item.getActionView());
+            Utils.tellAboutUs(this,null);
         } else if (id == R.id.nav_send) {
 
         }
@@ -118,8 +141,8 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    public void startAboutActivity() {
-        Intent i = new Intent(this, About.class);
+    private void startAboutActivity() {
+        Intent i = new Intent(this,About.class);
         startActivity(i);
     }
 
@@ -208,5 +231,95 @@ public class MainActivity extends AppCompatActivity
             }
         }
     }
+
+    /**
+     * This function creates a pop-up window with user diagnose
+     * @param title - String
+     * @param body - String: The content of pop up
+     * @param precentage - double: The prediction of melanoma
+     * @param level - int (-1,0,1) The severity of diagnose. 1-Good, 0-medium risk, (-1)-Bad
+     */
+    private void showPopUp(String title,String body, double precentage, int level){
+        switch (level){
+            case -1:
+                  new DroidDialog.Builder(this)
+                        .icon(R.drawable.diagnose_bad)
+                        .title(title)
+                        .content(body)
+                        .color(ContextCompat.getColor(this, R.color.diagnose_bad),0,ContextCompat.getColor(this, R.color.diagnose_bad))
+                        .positiveButton(getResources().getString(R.string.pop_up_close_window), new DroidDialog.onPositiveListener() {
+                            @Override
+                            public void onPositive(Dialog dialog) {
+                                dialog.dismiss();
+                                //TODO: this point is after getting diagnose. we save here the result (save date+ diagnose and "write" on image). and prepare the app for new cycle
+
+                            }
+                        })
+                        .animation(AnimUtils.AnimFadeInOut)
+                        .show();
+                break;
+            case 0:
+                new DroidDialog.Builder(this)
+                        .icon(R.drawable.diagnose_medium_risk)
+                        .title(title)
+                        .content(body)
+                        .color(ContextCompat.getColor(this, R.color.diagnose_meduim),0,ContextCompat.getColor(this, R.color.diagnose_meduim))
+                        .positiveButton(getResources().getString(R.string.pop_up_close_window), new DroidDialog.onPositiveListener() {
+                            @Override
+                            public void onPositive(Dialog dialog) {
+                                dialog.dismiss();
+                                //TODO: this point is after getting diagnose. we save here the result (save date+ diagnose and "write" on image). and prepare the app for new cycle
+
+                            }
+                        })
+                        .animation(AnimUtils.AnimFadeInOut)
+                        .show();
+                break;
+
+
+            case 1:
+                  new DroidDialog.Builder(this)
+                        .icon(R.drawable.diagnose_good)
+                        .title(title)
+                        .content(body)
+                        .color(ContextCompat.getColor(this, R.color.diagnose_ok),0,ContextCompat.getColor(this, R.color.diagnose_ok))
+                        .positiveButton(getResources().getString(R.string.pop_up_close_window), new DroidDialog.onPositiveListener() {
+                            @Override
+                            public void onPositive(Dialog dialog) {
+                                dialog.dismiss();
+                                //TODO: this point is after getting diagnose. we save here the result (save date+ diagnose and "write" on image). and prepare the app for new cycle
+
+                            }
+                        })
+                        .animation(AnimUtils.AnimFadeInOut)
+                        .show();
+                break;
+
+        }
+
+    }
+
+    private void showCase(String msg){
+        View camera = findViewById(R.id.content_camera);
+        View gallery = findViewById(R.id.content_gallery);
+        View history = findViewById(R.id.content_history);
+        View links = findViewById(R.id.content_link);
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(500); // half second between each showcase view
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this,msg);
+        //sequence.setConfig(config);
+
+        sequence.addSequenceItem(camera,
+                getResources().getString(R.string.show_case_camera_text), getResources().getString(R.string.show_case_close_window));
+        sequence.addSequenceItem(gallery,
+                getResources().getString(R.string.show_case_gallery_text), getResources().getString(R.string.show_case_close_window));
+        sequence.addSequenceItem(history,
+                getResources().getString(R.string.show_case_history_text), getResources().getString(R.string.show_case_close_window));
+        sequence.addSequenceItem(links,
+                getResources().getString(R.string.show_case_links_text), getResources().getString(R.string.show_case_close_window));
+        sequence.start();
+    }
+    
 }
 
