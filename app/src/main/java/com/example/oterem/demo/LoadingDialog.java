9499a99f5ab57ae3241d3051f6be1ab8d8ed3f85
Omri Terem <oterem@cisco.com>
2018-05-29 13:58:01 +0300

    adding loadingDialog class

---
+++ app/src/main/java/com/example/oterem/demo/LoadingDialog.java
@@ -0,0 +1,39 @@
+package com.example.oterem.demo;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by oterem on 29/05/2018.
+ */
+
+public class LoadingDialog extends AppCompatActivity {
+
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog(String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(message);
+            mProgressDialog.setCanceledOnTouchOutside(false);
+            mProgressDialog.setIndeterminate(true);
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+
+}
