c2d88caa19637e6101a835b5f02cd4e85abe617a
shail <shai.work1@gmail.com>
2018-05-30 13:00:43 +0300

    after adding AWS code

--- app/src/main/java/com/example/oterem/demo/Utils.java
+++ app/src/main/java/com/example/oterem/demo/Utils.java
@@ -4,7 +4,13 @@ package com.example.oterem.demo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -26,7 +32,7 @@ public abstract class Utils {
                 ".jpg",         /* suffix */
                 storageDir      /* directory */
         );
-        //imageName = imageFileName;
+        String imageName = imageFileName;
         // Save a file: path for use with ACTION_VIEW intents
 //        currentPhotoPath = image.getAbsolutePath();
         return image;
@@ -44,4 +50,83 @@ public abstract class Utils {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 
+    public static String getPath(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        Log.i("URI", uri + "");
+        String result = uri + "";
+        // DocumentProvider
+        //  if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+        if (isKitKat && (result.contains("media.documents"))) {
+            String[] ary = result.split("/");
+            int length = ary.length;
+            String imgary = ary[length - 1];
+            final String[] dat = imgary.split("%3A");
+            final String docId = dat[1];
+            final String type = dat[0];
+            Uri contentUri = null;
+            if ("image".equals(type)) {
+                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            } else if ("video".equals(type)) {
+            } else if ("audio".equals(type)) {
+            }
+            final String selection = "_id=?";
+            final String[] selectionArgs = new String[]{
+                    dat[1]
+            };
+            return getDataColumn(context, contentUri, selection, selectionArgs);
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static File getTempFile(Context context) {
+        File file = new File("");
+        String name;
+        try {
+            String fileName = "omri";
+            file = File.createTempFile(fileName, ".json", context.getCacheDir());
+            String path = file.getAbsolutePath();
+            name = parseImageName(path);
+
+        } catch (IOException e) {
+            // Error while creating file
+        }
+        return file;
+    }
+
+    private static String parseImageName(String path) {
+        String[] tokens = path.split("/");
+        String name = tokens[tokens.length - 1];
+        String n = name.replace(".jpg", "");
+        return name;
+    }
+
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null;
+    }
 }
